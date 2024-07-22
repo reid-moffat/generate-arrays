690b36710b2e74138da11b9fcfe527029af5b918
Reid Moffat <18rem8@queensu.ca>
2024-07-22 10:33:22 -0400

    simple version

---
+++ index.ts
@@ -0,0 +1,9 @@
+/**
+ * Generate an array of random numbers from [0, 100)
+ * @param size size of the array
+ */
+const generateArray = (size: number) => {
+    return Array.from({ length: size }, () => Math.floor(Math.random() * 100));
+}
+
+export default generateArray;
