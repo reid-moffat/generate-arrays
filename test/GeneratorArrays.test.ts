0610bdeac13e7aa56fa10187c3a9e22669294f42
Reid Moffat <18rem8@queensu.ca>
2024-07-31 16:45:12 -0400

    generateArray runtime errors

--- test/GeneratorArrays.test.ts
+++ test/GeneratorArrays.test.ts
@@ -4,33 +4,41 @@ import { expect } from "chai";
 suite("Generator array functions", () => {
 
     suite("Equal chances", () => {
+
+        const validGenerators = [
+            () => Math.random(),
+            () => Math.floor(Math.random() * 100) + 1
+        ];
+
         suite("Invalid input", () => {
             test("Length not an integer", () => {
-                expect(() => GenerateArray.generators(1.2, 1)).to.throw("Array length must be an integer: value '1.2' is" +
-                    " invalid");
+                expect(() => GenerateArray.generators(1.2, validGenerators)).to.throw("Array length must be an" +
+                    " integer: value '1.2' is invalid");
             });
 
-            test("Length less than 1", () => {
-                expect(() => GenerateArray.generators(0, 1)).to.throw("Array length must be greater than 0 to generate array");
+            test("Length of 0", () => {
+                expect(() => GenerateArray.generators(0, validGenerators)).to.throw("Array length must be greater" +
+                    " than 0 to generate array");
             });
 
-            test("Depth not an integer", () => {
-                expect(() => GenerateArray.generators(1, 1.2)).to.throw("Array depth must be an integer: value '1.2' is invalid");
+            test("Negative length", () => {
+                expect(() => GenerateArray.generators(-7, validGenerators)).to.throw("Array length must be greater" +
+                    " than 0 to generate array");
             });
 
-            test("Depth less than 1", () => {
-                expect(() => GenerateArray.generators(1, 0)).to.throw("Array depth must be at least 1 to generate array");
+            test("No generators", () => {
+                expect(() => GenerateArray.generators(1, [])).to.throw("No generators provided to create array");
             });
         });
 
         suite("Valid input", () => {
             test("Length 1 Depth 1", () => {
-                const arr = GenerateArray.generators(1, 1);
+                const arr = GenerateArray.generators(1, validGenerators);
                 expect(arr).to.deep.equal([1]);
             });
 
             test("Length 3 Depth 3", () => {
-                const arr = GenerateArray.generators(3, 3);
+                const arr = GenerateArray.generators(3, validGenerators);
                 expect(arr.length).to.equal(3);
                 expect(arr[0].length).to.equal(3);
                 // @ts-ignore
@@ -41,33 +49,44 @@ suite("Generator array functions", () => {
     });
 
     suite("Weighted chances", () => {
+
+        const validWeightedGenerators = [
+            { generator: () => Math.random(), chance: 0.5 },
+            { generator: () => Math.random() + 1, chance: 0.14 },
+            { generator: () => Math.random() + 2, chance: 0.36 }
+        ];
+
         suite("Invalid input", () => {
             test("Length not an integer", () => {
-                expect(() => GenerateArray.weightedGenerators(1.2, 1)).to.throw("Array length must be an integer: value '1.2' is" +
+                expect(() => GenerateArray.weightedGenerators(1.2, validWeightedGenerators)).to.throw("Array length must be an integer:" +
+                    " value '1.2' is" +
                     " invalid");
             });
 
             test("Length less than 1", () => {
-                expect(() => GenerateArray.weightedGenerators(0, 1)).to.throw("Array length must be greater than 0 to generate array");
+                expect(() => GenerateArray.weightedGenerators(0, validWeightedGenerators)).to.throw("Array length must be greater than 0" +
+                    " to generate array");
             });
 
             test("Depth not an integer", () => {
-                expect(() => GenerateArray.weightedGenerators(1, 1.2)).to.throw("Array depth must be an integer: value '1.2' is invalid");
+                expect(() => GenerateArray.weightedGenerators(1, validWeightedGenerators)).to.throw("Array depth must be an integer:" +
+                    " value '1.2' is invalid");
             });
 
             test("Depth less than 1", () => {
-                expect(() => GenerateArray.weightedGenerators(1, 0)).to.throw("Array depth must be at least 1 to generate array");
+                expect(() => GenerateArray.weightedGenerators(1, validWeightedGenerators)).to.throw("Array depth must be at least 1 to" +
+                    " generate array");
             });
         });
 
         suite("Valid input", () => {
             test("Length 1 Depth 1", () => {
-                const arr = GenerateArray.weightedGenerators(1, 1);
+                const arr = GenerateArray.weightedGenerators(1, validWeightedGenerators);
                 expect(arr).to.deep.equal([1]);
             });
 
             test("Length 3 Depth 3", () => {
-                const arr = GenerateArray.weightedGenerators(3, 3);
+                const arr = GenerateArray.weightedGenerators(3, validWeightedGenerators);
                 expect(arr.length).to.equal(3);
                 expect(arr[0].length).to.equal(3);
                 // @ts-ignore
@@ -78,32 +97,41 @@ suite("Generator array functions", () => {
     });
 
     suite("Fixed count", () => {
+
+        const validCountedGenerators = [
+            { generator: () => Math.random(), count: 2 },
+            { generator: () => Math.random() + 1, count: 7 },
+            { generator: () => Math.random() + 2, count: 3 }
+        ];
+
         suite("Invalid input", () => {
             test("Length not an integer", () => {
-                expect(() => GenerateArray.fixedCountGenerators(1.2, 1)).to.throw("Array length must be an integer: value '1.2' is invalid");
+                expect(() => GenerateArray.fixedCountGenerators(1.2, validCountedGenerators)).to.throw("Array length must be an integer:" +
+                    " value '1.2' is invalid");
             });
 
             test("Length less than 1", () => {
-                expect(() => GenerateArray.fixedCountGenerators(0, 1)).to.throw("Array length must be greater than 0 to generate array");
+                expect(() => GenerateArray.fixedCountGenerators(0, validCountedGenerators)).to.throw("Array length must be greater than" +
+                    " 0 to generate array");
             });
 
             test("Depth not an integer", () => {
-                expect(() => GenerateArray.fixedCountGenerators(1, 1.2)).to.throw("Array depth must be an integer: value '1.2' is invalid");
+                expect(() => GenerateArray.fixedCountGenerators(1, validCountedGenerators)).to.throw("Array depth must be an integer: value '1.2' is invalid");
             });
 
             test("Depth less than 1", () => {
-                expect(() => GenerateArray.fixedCountGenerators(1, 0)).to.throw("Array depth must be at least 1 to generate array");
+                expect(() => GenerateArray.fixedCountGenerators(1, validCountedGenerators)).to.throw("Array depth must be at least 1 to generate array");
             });
         });
 
         suite("Valid input", () => {
             test("Length 1 Depth 1", () => {
-                const arr = GenerateArray.fixedCountGenerators(1, 1);
+                const arr = GenerateArray.fixedCountGenerators(1, validCountedGenerators);
                 expect(arr).to.deep.equal([1]);
             });
 
             test("Length 3 Depth 3", () => {
-                const arr = GenerateArray.fixedCountGenerators(3, 3);
+                const arr = GenerateArray.fixedCountGenerators(3, validCountedGenerators);
                 expect(arr.length).to.equal(3);
                 expect(arr[0].length).to.equal(3);
                 // @ts-ignore
