46d2cfca0ae121de094e48c41ef65f8c20b0e19e
Reid Moffat <18rem8@queensu.ca>
2024-07-31 12:13:20 -0400

    moved multi-d into main class

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import GenerateArray from './GenerateArray.ts';
-import Multidimensional from "./MultidimensionalArray.ts";
 import CustomArray from "./CustomArrayGen.ts";
 import ArrayUtils from "./ArrayUtils.ts";
 import GenerateArrayError from "./GenerateArraysError.ts";
 
-export { GenerateArray, Multidimensional, CustomArray, ArrayUtils, GenerateArrayError };
+export { GenerateArray, CustomArray, ArrayUtils, GenerateArrayError };
