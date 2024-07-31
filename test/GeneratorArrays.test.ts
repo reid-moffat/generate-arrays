c550625284b5fa711c2512eff998eb9713516c7e
Reid Moffat <18rem8@queensu.ca>
2024-07-31 17:25:02 -0400

    equal chance TCs

--- test/GeneratorArrays.test.ts
+++ test/GeneratorArrays.test.ts
@@ -12,38 +12,35 @@ suite("Generator array functions", () => {
 
         suite("Invalid input", () => {
             test("Length not an integer", () => {
-                expect(() => GenerateArray.generators(1.2, validGenerators)).to.throw("Array length must be an" +
-                    " integer: value '1.2' is invalid");
+                expect(() => GenerateArray.generators(1.2, validGenerators)).to.throw("Parameter 'length' must be an integer: value '1.2' is invalid");
             });
 
             test("Length of 0", () => {
-                expect(() => GenerateArray.generators(0, validGenerators)).to.throw("Array length must be greater" +
-                    " than 0 to generate array");
+                expect(() => GenerateArray.generators(0, validGenerators)).to.throw("Parameter 'length' must be at least 1: value '0' is invalid");
             });
 
             test("Negative length", () => {
-                expect(() => GenerateArray.generators(-7, validGenerators)).to.throw("Array length must be greater" +
-                    " than 0 to generate array");
+                expect(() => GenerateArray.generators(-7, validGenerators)).to.throw("Parameter 'length' must be at least 1: value '-7' is invalid");
             });
 
             test("No generators", () => {
-                expect(() => GenerateArray.generators(1, [])).to.throw("No generators provided to create array");
+                expect(() => GenerateArray.generators(1, [])).to.throw("generators[Math.floor(...)] is not a function");
             });
         });
 
         suite("Valid input", () => {
             test("Length 1 Depth 1", () => {
                 const arr = GenerateArray.generators(1, validGenerators);
-                expect(arr).to.deep.equal([1]);
+                expect(arr.length).to.equal(1);
+                expect(arr[0]).to.be.an("number");
             });
 
             test("Length 3 Depth 3", () => {
                 const arr = GenerateArray.generators(3, validGenerators);
                 expect(arr.length).to.equal(3);
-                expect(arr[0].length).to.equal(3);
-                // @ts-ignore
-                expect(arr[0][0].length).to.equal(0);
-                expect(arr).to.deep.equal([[[], [], []], [[], [], []],[[], [], []]]);
+                expect(arr[0]).to.be.an("number");
+                expect(arr[1]).to.be.an("number");
+                expect(arr[2]).to.be.an("number");
             });
         });
     });
