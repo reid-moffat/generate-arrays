3455a0db154ba07d85c1c08c036c229f70693a99
Reid Moffat <18rem8@queensu.ca>
2024-07-31 14:15:32 -0400

    moved custom arrays to main class

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import GenerateArray from './GenerateArray.ts';
-import CustomArray from "./CustomArray.ts";
 import ArrayUtils from "./ArrayUtils.ts";
 import GenerateArrayError from "./GenerateArrayError.ts";
 
-export { GenerateArray, CustomArray, ArrayUtils, GenerateArrayError };
+export { GenerateArray, ArrayUtils, GenerateArrayError };
