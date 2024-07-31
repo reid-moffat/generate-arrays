057d5c3bad05c3a258d2f05ad58380a39ac61f70
Reid Moffat <18rem8@queensu.ca>
2024-07-31 16:35:32 -0400

    update multi-d object call

--- test/MultiDimensional.test.ts
+++ test/MultiDimensional.test.ts
@@ -1,36 +1,37 @@
 import { GenerateArray } from "../src/index.ts";
 import { expect } from "chai";
 
-suite("Multi-dimensional array test", () => {
+suite("Multi-dimensional array functions", () => {
 
     suite("Empty base array", () => {
 
         suite("Invalid input", () => {
             test("Length not an integer", () => {
-                expect(() => MultidimensionalArray.empty(1.2, 1)).to.throw("Array length must be an integer: value '1.2' is invalid");
+                expect(() => GenerateArray.emptyND(1.2, 1)).to.throw("Array length must be an integer: value '1.2' is" +
+                    " invalid");
             });
 
             test("Length less than 1", () => {
-                expect(() => MultidimensionalArray.empty(0, 1)).to.throw("Array length must be greater than 0 to generate array");
+                expect(() => GenerateArray.emptyND(0, 1)).to.throw("Array length must be greater than 0 to generate array");
             });
 
             test("Depth not an integer", () => {
-                expect(() => MultidimensionalArray.empty(1, 1.2)).to.throw("Array depth must be an integer: value '1.2' is invalid");
+                expect(() => GenerateArray.emptyND(1, 1.2)).to.throw("Array depth must be an integer: value '1.2' is invalid");
             });
 
             test("Depth less than 1", () => {
-                expect(() => MultidimensionalArray.empty(1, 0)).to.throw("Array depth must be at least 1 to generate array");
+                expect(() => GenerateArray.emptyND(1, 0)).to.throw("Array depth must be at least 1 to generate array");
             });
         });
 
         suite("Valid input", () => {
             test("Length 1 Depth 1", () => {
-                const arr = MultidimensionalArray.empty(1, 1);
+                const arr = GenerateArray.emptyND(1, 1);
                 expect(arr).to.deep.equal([]);
             });
 
             test("Length 3 Depth 3", () => {
-                const arr = MultidimensionalArray.empty(3, 3);
+                const arr = GenerateArray.emptyND(3, 3);
                 expect(arr.length).to.equal(3);
                 expect(arr[0].length).to.equal(3);
                 // @ts-ignore
@@ -44,45 +45,46 @@ suite("Multi-dimensional array test", () => {
 
         suite("Invalid input", () => {
             test("Length not an integer", () => {
-                expect(() => MultidimensionalArray.uniform(7, 1.2, 1)).to.throw("Array length must be an integer: value '1.2' is invalid");
+                expect(() => GenerateArray.uniformND(7, 1.2, 1)).to.throw("Array length must be an integer: value" +
+                    " '1.2' is invalid");
             });
 
             test("Length less than 1", () => {
-                expect(() => MultidimensionalArray.uniform(7, 0, 1)).to.throw("Array length must be greater than 0 to generate array");
+                expect(() => GenerateArray.uniformND(7, 0, 1)).to.throw("Array length must be greater than 0 to generate array");
             });
 
             test("Depth not an integer", () => {
-                expect(() => MultidimensionalArray.uniform(7, 1, 1.2)).to.throw("Array depth must be an integer: value '1.2' is invalid");
+                expect(() => GenerateArray.uniformND(7, 1, 1.2)).to.throw("Array depth must be an integer: value '1.2' is invalid");
             });
 
             test("Depth less than 1", () => {
-                expect(() => MultidimensionalArray.uniform(7, 1, 0)).to.throw("Array depth must be at least 1 to generate array");
+                expect(() => GenerateArray.uniformND(7, 1, 0)).to.throw("Array depth must be at least 1 to generate array");
             });
         });
 
         suite("Valid input", () => {
             test("Value 7 Length 1 Depth 1", () => {
-                const arr = MultidimensionalArray.uniform(7, 1, 1);
+                const arr = GenerateArray.uniformND(7, 1, 1);
                 expect(arr).to.deep.equal([7]);
             });
 
             test("Value 7 Length 3 Depth 1", () => {
-                const arr = MultidimensionalArray.uniform(7, 3, 1);
+                const arr = GenerateArray.uniformND(7, 3, 1);
                 expect(arr).to.deep.equal([7]);
             });
 
             test("Value [1, 2, 3] Length 3 Depth 1", () => {
-                const arr = MultidimensionalArray.uniform([1, 2, 3], 3, 1);
+                const arr = GenerateArray.uniformND([1, 2, 3], 3, 1);
                 expect(arr).to.deep.equal([1, 2, 3]);
             });
 
             test("Value 7 Length 3 Depth 2", () => {
-                const arr = MultidimensionalArray.uniform(7, 3, 2);
+                const arr = GenerateArray.uniformND(7, 3, 2);
                 expect(arr).to.deep.equal([[7], [7], [7]]);
             });
 
             test("Value [1, 2, 3] Length 3 Depth 2", () => {
-                const arr = MultidimensionalArray.uniform([1, 2, 3], 3, 2);
+                const arr = GenerateArray.uniformND([1, 2, 3], 3, 2);
                 expect(arr).to.deep.equal([[1, 2, 3], [1, 2, 3], [1, 2, 3]]);
             });
         });
@@ -92,36 +94,36 @@ suite("Multi-dimensional array test", () => {
 
         suite("Invalid input", () => {
             test("Length not an integer", () => {
-                expect(() => MultidimensionalArray.custom(() => [1, 2, 3], 1.2, 1)).to.throw("Array length must be an" +
+                expect(() => GenerateArray.customND(() => [1, 2, 3], 1.2, 1)).to.throw("Array length must be an" +
                     " integer: value '1.2' is invalid");
             });
 
             test("Length less than 1", () => {
-                expect(() => MultidimensionalArray.custom(() => [1, 2, 3], 0, 1)).to.throw("Array length must be greater than 0 to generate array");
+                expect(() => GenerateArray.customND(() => [1, 2, 3], 0, 1)).to.throw("Array length must be greater than 0 to generate array");
             });
 
             test("Depth not an integer", () => {
-                expect(() => MultidimensionalArray.custom(() => [1, 2, 3], 1, 1.2)).to.throw("Array depth must be an integer: value '1.2' is invalid");
+                expect(() => GenerateArray.customND(() => [1, 2, 3], 1, 1.2)).to.throw("Array depth must be an integer: value '1.2' is invalid");
             });
 
             test("Depth less than 1", () => {
-                expect(() => MultidimensionalArray.custom(() => [1, 2, 3], 1, 0)).to.throw("Array depth must be at least 1 to generate array");
+                expect(() => GenerateArray.customND(() => [1, 2, 3], 1, 0)).to.throw("Array depth must be at least 1 to generate array");
             });
         });
 
         suite("Valid input", () => {
             test("Value [1, 2, 3] Length 1 Depth 1", () => {
-                const arr = MultidimensionalArray.custom(() => [1, 2, 3], 1, 1);
+                const arr = GenerateArray.customND(() => [1, 2, 3], 1, 1);
                 expect(arr).to.deep.equal([1, 2, 3]);
             });
 
             test("Value [1, 2, 3] Length 3 Depth 1", () => {
-                const arr = MultidimensionalArray.custom(() => [1, 2, 3], 3, 1);
+                const arr = GenerateArray.customND(() => [1, 2, 3], 3, 1);
                 expect(arr).to.deep.equal([1, 2, 3]);
             });
 
             test("Value [1, 2, 3] Length 3 Depth 2", () => {
-                const arr = MultidimensionalArray.custom(() => [1, 2, 3], 3, 2);
+                const arr = GenerateArray.customND(() => [1, 2, 3], 3, 2);
                 expect(arr).to.deep.equal([[1, 2, 3], [1, 2, 3], [1, 2, 3]]);
             });
         });
