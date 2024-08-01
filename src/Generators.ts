849fa9ae243edcc8055191eae3a742939b7d804a
Reid Moffat <18rem8@queensu.ca>
2024-08-01 11:41:05 -0400

    sting gen improvements

--- src/Generators.ts
+++ src/Generators.ts
@@ -1,3 +1,6 @@
+import GenerateArrayError from "./GenerateArrayError.ts";
+import Validation from "./Validation.ts";
+
 const integer = (min: number = 0, max: number = 100) => {
     return () => Math.floor(Math.random() * (max - min + 1) + min);
 }
@@ -6,8 +9,34 @@ const decimal = (min: number = 0, max: number = 100, precision: number = 5) => {
     return () => (Math.random() * (max - min) + min).toFixed(precision);
 }
 
-const string = (length: number = 10) => {
-    return () => Math.random().toString(36).substring(2, 2 + length);
+/**
+ * Generates a random string. By default, the string is a length of 10 and uses alphanumeric characters
+ *
+ * @param length Length of the string as a set number or a range [min, max]
+ * @param specialChars True to include special characters
+ */
+const string = (length: number | [number, number] = 10, specialChars: boolean = false) => {
+
+    // ASCII min/max
+    const min = specialChars ? 33 : 48;
+    const max = specialChars ? 126 : 122;
+
+    // Variable length
+    if (Array.isArray(length)) {
+        if (length.length !== 2) {
+            throw new GenerateArrayError('Length range must be an array of two numbers: [min, max]');
+        }
+        Validation.integer(length[0], 0, 'Length min');
+        Validation.integer(length[1], min, 'Length max');
+
+        return () => { // @ts-ignore
+            length = Math.floor(Math.random() * (length[1] - length[0] + 1) + length[0]);
+            return Array.from({ length }, () => String.fromCharCode(Math.floor(Math.random() * (max - min + 1)) + min)).join('');
+        }
+    }
+
+    // @ts-ignore
+    return () => Array.from({ length }, () => String.fromCharCode(Math.floor(Math.random() * (max - min + 1)) + min)).join('');
 }
 
 export { integer, decimal, string };
