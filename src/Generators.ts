f11b55f7392d611f2ce5ee1979787a480297ef4b
Reid Moffat <18rem8@queensu.ca>
2024-08-01 13:27:46 -0400

    int & decimal documentation

--- src/Generators.ts
+++ src/Generators.ts
@@ -1,18 +1,31 @@
 import GenerateArrayError from "./GenerateArrayError.ts";
 import Validation from "./Validation.ts";
 
+/**
+ * Generator for a random integer. By default, the number is between 0 and 100 (both inclusive)
+ *
+ * @param min Minimum value
+ * @param max Maximum value
+ */
 const integer = (min: number = 0, max: number = 100) => {
     return () => Math.floor(Math.random() * (max - min + 1) + min);
 }
 
+/**
+ * Generator for a random decimal number. By default, the number is between 0 and 100 with a precision of 5
+ *
+ * @param min Minimum value
+ * @param max Maximum value
+ * @param precision Number of decimal places
+ */
 const decimal = (min: number = 0, max: number = 100, precision: number = 5) => {
     return () => (Math.random() * (max - min) + min).toFixed(precision);
 }
 
 /**
- * Generates a random string. By default, the string is a length of 10 and uses alphanumeric characters
+ * Generator for a random string. By default, the string is a length of 10 and uses alphanumeric characters
  *
- * @param length Length of the string as a set number or a range [min, max]
+ * @param length Length of the string as a set number or a range [min, max] (both inclusive)
  * @param specialChars True to include special characters
  */
 const string = (length: number | [number, number] = 10, specialChars: boolean = false) => {
