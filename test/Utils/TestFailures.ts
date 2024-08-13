83adfbce1d4425a94633dea9f33f7bddf41bfd83
Reid Moffat <18rem8@queensu.ca>
2024-08-13 13:57:47 -0400

    updated logic + structure for tests

--- test/Utils/TestFailures.ts
+++ test/Utils/TestFailures.ts
@@ -6,42 +6,33 @@ class TestFailures {
 
     private readonly path: string[];
     private readonly func: Function;
-    private readonly parameterTests: Map<string, Parameter> = new Map<string, Parameter>();
+    private readonly parameters: Parameter[];
 
     constructor(path: string[], func: Function, parameters: Parameter | Parameter[]) {
         this.path = path;
         this.func = func;
+        this.parameters = Array.isArray(parameters) ? parameters : [parameters];
     }
 
     public runTests(): void {
-        // const testValues = new Map<string, any>();
-        // this.parameters.forEach((constraints, name) => {
-        //     testValues.set(name, undefined);
-        //     testValues.set(name, null);
-        //     testValues.set(name, "");
-        //     testValues.set(name, []);
-        //     testValues.set(name, {});
-        // });
 
-        const tests = [];
-        tests.push(
-            () => {
-                const pathWithTest = this.path.slice();
-                pathWithTest.push(`generated test: ${undefined}`);
+        for (const parameter of this.parameters) {
+            for (const [name, value] of parameter.getValues()) {
+                test(name, () => {
+                    const pathWithTest = [...this.path.slice(), name];
 
-                try {
-                    TestFailures.TestTimer.startTest(pathWithTest);
-                    this.func(undefined);
-                } catch (error) {
-                    TestFailures.TestTimer.stopTest();
-                    console.log(JSON.stringify(error, null, 4));
-                }
+                    let error;
+                    try {
+                        TestFailures.TestTimer.startTest(pathWithTest);
+                        this.func(value);
+                    } catch (err: any) {
+                        TestFailures.TestTimer.stopTest();
+                        error = err;
+                        console.log(JSON.stringify(error, null, 4));
+                    }
+                });
             }
-        );
-
-        tests.forEach(t => {
-            test("test failure test in class", () => t());
-        });
+        }
     }
 }
 
@@ -51,6 +42,17 @@ abstract class Parameter {
 
     protected constructor(name: string) {
         this.name = name;
+    }
+
+    public abstract getValues(): Map<string, any>;
+
+    private static makeValuesMap(paramName: string, values: any[]): Map<string, any> {
+        return new Map<string, any>(values.map((value: any) => [`${paramName}: ${JSON.stringify(value)}`, value]));
+    }
+
+    protected static nonNumbers(paramName: string): Map<string, any> {
+        const values: any[] = [undefined, null, "", [], {}];
+        return Parameter.makeValuesMap(paramName, values);
     }
 }
 
@@ -65,6 +67,10 @@ class NumberParameter extends Parameter {
         this.min = min ?? null;
         this.max = max ?? null;
         this.integer = integer;
+    }
+
+    public getValues(): Map<string, any> {
+        return Parameter.nonNumbers(this.name);
     }
 }
 
