308f3ac16d6efe93e36f7a87a5a671e20eece292
Reid Moffat <18rem8@queensu.ca>
2024-08-14 20:50:43 -0400

    throw error for invlaid suite syntax

--- test/Utils/Utils.ts
+++ test/Utils/Utils.ts
@@ -2,6 +2,12 @@
  * Gets the path of the current suite
  */
 const getPath = function(suite: any) {
+
+    if (suite === undefined) {
+        throw new Error("Suite is undefined in getPath - make sure the suite this is being called from is using the" +
+            " function() {} notation, not () => {}");
+    }
+
     const suites = [];
     let currentSuite = suite;
 
