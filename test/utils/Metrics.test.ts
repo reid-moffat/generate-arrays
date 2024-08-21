59cba8d2cf1f6f87b54ddfdd61c5351849d2dcb5
Reid Moffat <18rem8@queensu.ca>
2024-08-21 14:39:12 -0400

    remove arrow notation from metrics tests

--- test/utils/Metrics.test.ts
+++ test/utils/Metrics.test.ts
@@ -2,16 +2,16 @@ import SuiteMetrics from "suite-metrics";
 
 const TestTimer = SuiteMetrics.getInstance();
 
-suite("Test metrics", () => {
-    test("All suite metrics", () => {
+suite("Test metrics", function() {
+    test("All suite metrics", function() {
         console.log(TestTimer.printAllSuiteMetrics(true));
     });
 
-    test("All test metrics", () => {
+    test("All test metrics", function() {
         console.log(TestTimer.getSuiteMetrics([]));
     });
 
-    test("All test metrics recursive", () => {
+    test("All test metrics recursive", function() {
         console.log(TestTimer.getSuiteMetricsRecursive([]));
     });
 });
