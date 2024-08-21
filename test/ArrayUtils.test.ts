e7c25c2e9cb2286d076881275c40ab50bf66c6b5
Reid Moffat <18rem8@queensu.ca>
2024-08-21 10:06:25 -0400

    remove duplicates test + print helper

--- test/ArrayUtils.test.ts
+++ test/ArrayUtils.test.ts
@@ -1,6 +1,6 @@
 import { expect } from "chai";
 import SuiteMetrics from "suite-metrics";
-import { getPath } from "./utils/Utils.ts";
+import { getPath, printArray } from "./utils/Utils.ts";
 import ArrayUtils from "../src/functions/ArrayUtils.ts";
 import {
     ArrayParameter,
@@ -59,5 +59,47 @@ suite("Array Utils", () => {
             parameters: new ArrayParameter("arr")
         }
         TestFailures.run(failureTestData);
+
+        suite("Valid input", function() {
+
+            const _test = (name: string, arr: any[], expected: any[]) => {
+                test(name, () => {
+
+                    console.log(`Running test: ${name}`);
+                    console.log(`Flattening array: ${printArray(arr)}`);
+
+                    TestTimer.startTest(getPath(this).concat(name));
+                    const result = ArrayUtils.removeDuplicates(arr);
+                    TestTimer.stopTest();
+
+                    console.log(`Result: ${printArray(result)}`);
+                    console.log(`Verifying it equals ${printArray(expected)}...`);
+                    expect(result).to.deep.equal(expected);
+                    console.log("Success!\n");
+                });
+            }
+
+            _test("Empty array", [], []);
+
+            _test("Single element", [1], [1]);
+
+            _test("No duplicates", [1, 2, 3], [1, 2, 3]);
+
+            _test("No duplicates #2", [7, 23, 2], [7, 23, 2]);
+
+            _test("One duplicate", [2, 1, 3, 2], [2, 1, 3]);
+
+            _test("One duplicate #2", [7, 6, 6], [7, 6]);
+
+            _test("One duplicate #3", [12, 12], [12]);
+
+            _test("Multiple duplicates", [1, 2, 3, 1, 2, 3], [1, 2, 3]);
+
+            _test("Multiple duplicates #2", [9, 4, 23, 2, 4, 7, 8, 7, 23, 91, 83, 8, 2], [9, 4, 23, 2, 7, 8, 91, 83]);
+
+            _test("Many of the same value", [1, 1, 1, 1, 1], [1]);
+
+            _test("10 million of the same value", Array(10_000_000).fill(827), [827]);
+        });
     });
 });
--- test/utils/Utils.ts
+++ test/utils/Utils.ts
@@ -1,7 +1,7 @@
 import * as Mocha from 'mocha';
 
 /**
- * Gets the path of the current suite
+ * Gets the path of the current suite for TestMetrics
  */
 const getPath = function(suite: Mocha.Suite | Mocha.Context): string[] {
 
@@ -21,4 +21,15 @@ const getPath = function(suite: Mocha.Suite | Mocha.Context): string[] {
     return suites;
 };
 
-export { getPath };
+/**
+ * Prints an array in a clear format up to the first 100 elements to not clog the console
+ */
+const printArray = (arr: any[]) => {
+    if (arr.length < 100) {
+        return JSON.stringify(arr);
+    }
+
+    return "[" + arr.slice(0, 100).join(", ") + `, ... (${arr.length - 100} more elements)]`;
+}
+
+export { getPath, printArray };
